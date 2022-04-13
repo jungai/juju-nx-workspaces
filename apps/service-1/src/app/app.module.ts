8e7941c62a3f2223da79242f14223f3af5336f9a
jungai <juworaphol125@gmail.com>
2022-04-13 15:24:20 +0700

    init with nx app

--- apps/service-1/src/app/app.module.ts
+++ apps/service-1/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UtilsPrismaModule } from '@juju-nx-workspace/utils/prisma';
 
 @Module({
-  imports: [],
+  imports: [UtilsPrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/service-1/src/app/app.service.ts
+++ apps/service-1/src/app/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { UtilsPrismaService } from '@juju-nx-workspace/utils/prisma';
+import { Game } from '@juju-nx-workspace/enum';
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Welcome to service-1!' };
+  constructor(private prisma: UtilsPrismaService) {}
+
+  async getData() {
+    console.log('BEST GAME -> ', Game.VALO);
+    return await this.prisma.song.findMany();
   }
 }
